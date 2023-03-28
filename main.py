dd0d63d7667ae0f9831ecd30f24fc43200185bf3
Evan Snyder <evan@larky.com>
2023-03-28 13:38:00 -0700

    Draft of part three completed; unifying parts and switching to csv read

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import pandas as pd
 import datetime
 from sodapy import Socrata
 
-
 client = Socrata("healthdata.gov", None)
+now = datetime.datetime.now()
 
 def main():
+	part_one()
 	part_two()
+	part_three()
 
 def part_one():
 	df = pd.read_csv('data/Dataset.csv')
@@ -19,7 +21,7 @@ def part_one():
 
 	for state in unique_states:
 		state_results = df[df.state == state]
-		for outcome in unique_outcomes:
+		for outcome in unique_outcomes: # TODO: Is this loop necessary? Why not just sum everything?
 			# print(state, outcome)
 			state_outcome_results = state_results[state_results.overall_outcome == outcome]
 			# print(len(outcome))
@@ -28,21 +30,22 @@ def part_one():
 				# print(state_outcome_results.iloc[0]['total_results_reported'])
 				# print(df.query(f'state == "{state}" & overall_outcome == "{outcome}"').iloc[0]['total_results_reported'])
 
-	print('total tests as of yesterday', total_tests)
+	print('Total tests as of yesterday:', total_tests)
 
 AVG_LENGTH = 7
 NUMBER_OF_DAYS = 30
 def part_two():
 	# We start calculating on the seventh day, so subtract one
 	dataset_length = NUMBER_OF_DAYS - 1 + AVG_LENGTH
-	now = datetime.datetime.now()
 	oldest_date = now - datetime.timedelta(days=dataset_length)
-
 	datestr = f'{oldest_date.year}-{oldest_date.month}-{oldest_date.day}' 
 
-	results = client.get("j8mb-icvb", where=f'date >= "{datestr}"', limit=10000)
+	df = pd.read_csv('data/Dataset.csv')
+	df = df[df.date > datestr]
 
-	df = pd.DataFrame.from_records(results)
+	# results = client.get("j8mb-icvb", where=f'date >= "{datestr}"', limit=10000)
+
+	# df = pd.DataFrame.from_records(results)
 
 	df['date'] = pd.to_datetime(df['date'])
 	df['new_results_reported'] = pd.to_numeric(df['new_results_reported'])
@@ -62,6 +65,32 @@ def part_two():
 			rolling_average_with_date.append((datestr, round(sum(total_new_case_queue) / AVG_LENGTH, 2)))
 			total_new_case_queue.pop(0)
 
-	print('Rolling average for the past 30 days', rolling_average_with_date)
+	print('Rolling average for the past 30 days:', rolling_average_with_date)
+
+NUMBER_OF_STATES = 10
+def part_three():
+	df = pd.read_csv('data/Dataset.csv')
+
+	oldest_date = now - datetime.timedelta(days=NUMBER_OF_DAYS) # TODO: Different variable?
+	datestr = f'{oldest_date.year}-{oldest_date.month}-{oldest_date.day}' 
+
+	df['date'] = pd.to_datetime(df['date'])
+	df['new_results_reported'] = pd.to_numeric(df['new_results_reported'])
+
+	df = df[df.date > datestr]
+
+	unique_states = df.state.unique()
+	positivity_rate_with_state = []
+
+	for state in unique_states:
+		state_data = df[df.state == state]
+		total_tests = state_data['new_results_reported'].sum()
+		positive = state_data[state_data.overall_outcome == 'Positive']['new_results_reported'].sum()
+
+		positivity_rate_with_state.append((state, round(positive / total_tests, 2)))
+	
+	positivity_rate_with_state.sort(key=lambda a: a[1], reverse=True)
+	print('Top ten positivity rates:', positivity_rate_with_state[:NUMBER_OF_STATES])
+
 
 main()
